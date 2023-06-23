f3dc258f0b2b4aec7934f70b9ea2b73ded9ffd96
1ogon1 <konovalenkoruslan@gmail.com>
2023-06-23 10:19:38 +0300

    fix config validation

--- src/config/config.validation.ts
+++ src/config/config.validation.ts
@@ -6,9 +6,9 @@ export const configSchemaValidation: Joi.ObjectSchema = Joi.object({
     .valid(...Object.values(EConfigEnvironment))
     .default(EConfigEnvironment.production),
   PORT: Joi.number().required(),
-  SWAGGER_USR: Joi.string().required(),
+  SWAGGER_USR: Joi.string().optional(),
   API_DOC_PATH: Joi.string().required(),
-  SWAGGER_PASSWORD: Joi.string().required(),
+  SWAGGER_PASSWORD: Joi.string().optional(),
   DB_CONNECTION_URL: Joi.string().required(),
   SECURITY_JWT_SECRET: Joi.string().required(),
   SECURITY_PASSWORD_SALT_ROUND: Joi.string().required(),
